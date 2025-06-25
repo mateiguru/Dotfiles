0797523a8a053acbe820ca38abae39aa2c803d79
guruleanu <113293838+mateiguru@users.noreply.github.com>
2025-06-25 23:51:46 +0300

    Add files via upload

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -16,9 +16,7 @@ echo ":: Setting wallpaper with original image $used_wallpaper"
 echo ":: Execute pywal with $used_wallpaper"
 wal -q -i "$used_wallpaper"
 
-wpg -s "$used_wallpaper" --noreload --noterminal
-
-walcord
+#wpg -s "$used_wallpaper" --noreload --noterminal
 
 # Source the generated color scheme
 source "$HOME/.cache/wal/colors.sh"
@@ -28,3 +26,7 @@ source "$HOME/.cache/wal/colors.sh"
 
 # Use swww to set the wallpaper
 swww img "$used_wallpaper"
+
+# Set firefox color scheme
+sudo pywalfox update
+
